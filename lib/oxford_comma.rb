0a5a5bd4280624428fb38651f08ac9e072e0edcd
Andrea Williams <anw0514@gmail.com>
2018-12-31 16:45:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   if array.length == 1
     str = array.join
     return str
-  else
-    return nil
+  elsif array.length == 2
+    str = array.join(" and ")
+    return str
   end
 end
